07c38c12bf532ee1a4d7757264b3d33042f5f342
LeonStaubachBreinify <leon.staubach@breinify.com>
2024-07-15 16:41:58 -0700

    fix

--- is_even.py
+++ is_even.py
@@ -1,8 +1,12 @@
 """
-Function that returns whether a given number or odd.
+Function that returns whether a given number or even.
 """
 def is_even(number: int) -> bool:
     return number % 2 == 0
 
+"""
+Function that returns whether a given number or odd.
+
+"""
 def is_odd(number: int) -> bool:
     return not is_even(number)
