bed649119510eb23b8643353701c5d3bff0b4157
LeonStaubachBreinify <leon.staubach@breinify.com>
2024-07-15 16:35:03 -0700

    added new is_odd function

--- is_even.py
+++ is_even.py
@@ -4,5 +4,5 @@ Function that returns whether a given number or odd.
 def is_even(number: int) -> bool:
     return number % 2 == 0
 
-
-print(is_even(123))
+def is_odd(number: int) -> bool:
+    return not is_even(number)
